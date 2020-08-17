f0b2b177ca34797830f17b4cbbba69c0f893ad25
Madhur Maurya <madhurmaurya365@gmail.com>
2020-08-17 22:25:07 +0530

    Fixed Errors

--- lib/data/service_locator.dart
+++ lib/data/service_locator.dart
@@ -1,5 +1,4 @@
 import 'package:get_it/get_it.dart';
-import 'package:news_app/controllers/tabs_controller.dart';
 import 'package:news_app/data/news_api.dart';
 import 'package:news_app/data/persistent_database.dart';
 
@@ -8,5 +7,4 @@ GetIt locator = GetIt.instance;
 void setupLocator() {
   locator.registerSingleton<NewsAPI>(NewsAPI.create());
   locator.registerSingleton<PersistentDatabase>(PersistentDatabase());
-  locator.registerSingleton<TabsController>(TabsController());
 }
